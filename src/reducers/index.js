fc178a0ae7fb7d9333cf18075e821ad53221ac3f
Daniel Engelhard <danielengelhard@Daniels-MacBook-Pro.local>
2021-02-23 02:13:06 -0800

    Added Full Time filter

--- src/reducers/index.js
+++ src/reducers/index.js
@@ -1,6 +1,8 @@
 import jobs from './jobs'
 import { combineReducers } from 'redux'
+import { loadingBarReducer } from 'react-redux-loading'
 
 export default combineReducers({
-  jobs
+  jobs,
+  loadingBarReducer
 })
--- src/utils/api.js
+++ src/utils/api.js
@@ -1,9 +1,14 @@
 import appendToGet from './appendToGet'
 import axios from 'axios'
 
+// removes all instances other than 'Full Time' since the API doesn't
+function onlyFullTime (data) {
+  return data.filter((inst) => inst.type === 'Full Time')
+}
+
 export async function searchForJobs ({ description, fulltime, location }) {
   const res = await axios.get(`http://localhost:3001/github/jobs${appendToGet({ description, location, full_time: fulltime })}`)
-  return res.data
+  return fulltime ? onlyFullTime(res.data) : res.data
 }
 
 export async function searchForJob (id) {
